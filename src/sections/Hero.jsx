52505f375d839a3f96b41395c7b1b0274b47f920
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-06-21 00:45:30 +0530

    second

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,23 +9,24 @@ import ShoeCard from "../components/ShoeCard";
 const Hero = () => {
 const [bigShoeImg, setbigShoeImg] = useState(bigShoe1)
 
+
   return (
     <section
       id="home"
       className="w-full flex xl:flex-row flex:col justify-center
-      min-h-screen gap-10 max-container "
+      min-h-screen gap-10 max-container"
     >
      <div className="relative xl:w-2/5 flex flex-col
      justify-center items-start w-full max-xl:padding-x pt-28 ">
       
-      <p className="text-xl font-montserrat text-purple-950 font-medium">Our Summer Collection</p>
+      <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
       <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px]
       max-sm:leading[82] font-bold">
-        <span className="xl:bg-violet-50 xl:whitespace-nowrap relative z-10 pr-10">
-        Where Stylish with
+        <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+        The New Arrival
         </span>
         <br />
-        <span className="text-fuchsia-500 inline-block mt-3">Lily</span> Clothes
+        <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
       </h1>
       <p className="font-montserrat text-slate-gray text-lg leading-8
       mt-6 mb-14 sm:max-w-sm">
